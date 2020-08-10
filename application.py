4183b6a0887f008195687fec82606b35a8904af3
sunnykriplani <kriplanisunny@gmail.com>
2020-08-10 14:45:42 +0100

    Update application.py

--- application.py
+++ application.py
@@ -1,9 +1,67 @@
-from flask import Flask
+from flask import Flask, request, redirect
+import requests
+
 app = Flask(__name__)
 
-@app.route("/")
-def hello():
-    return "Hello World!"
+CLIENT_ID = "3f034891bb6540cdf2bbbc19be0bdb7fb9d11800691a0043534fe7e7be995274"
+CUSTOMER_SECRET = "bca35e1f804aaaf8a089967d29abbf4259cf179f06e1c008949c0cf5d1ed057c"
+STATE = ""
+ACCOUNT_URL = "https://account.withings.com"
+WBSAPI_URL = "https://wbsapi.withings.net"
+CALLBACK_URI = "https://holistics.azurewebsites.net/get_token"
 
-if __name__ == '__main__':
-    app.run()
+
+@app.route("/")
+def get_code():
+    """
+    Route to get the permission from an user to take his data.
+    This endpoint redirects to a Withings' login page on which
+    the user has to identify and accept to share his data
+    """
+    payload = {'response_type': 'code',  # imposed string by the api
+               'client_id': CLIENT_ID,
+               'state': STATE,
+               'scope': 'user.info',  # see docs for enhanced scope
+               'redirect_uri': CALLBACK_URI,  # URL of this app
+               'mode': 'demo'  # Use demo mode, DELETE THIS FOR REAL APP
+               }
+
+    r_auth = requests.get(f'{ACCOUNT_URL}/oauth2_user/authorize2',
+                          params=payload)
+
+    return redirect(r_auth.url)
+
+
+@app.route("/get_token")
+def get_token():
+    """
+    Callback route when the user has accepted to share his data.
+    Once the auth has arrived Withings servers come back with
+    an authentication code and the state code provided in the
+    initial call
+    """
+    code = request.args.get('code')
+    state = request.args.get('state')
+
+    payload = {'grant_type': 'authorization_code',
+               'client_id': CLIENT_ID,
+               'client_secret': CUSTOMER_SECRET,
+               'code': code,
+               'redirect_uri': CALLBACK_URI
+               }
+
+    r_token = requests.post(f'{ACCOUNT_URL}/oauth2/token',
+                            data=payload).json()
+
+    access_token = r_token.get('access_token', '')
+
+    # GET Some info with this token
+    headers = {'Authorization': 'Bearer ' + access_token}
+    payload = {'action': 'getdevice'}
+
+    # List devices of returned user
+    r_getdevice = requests.get(f'{WBSAPI_URL}/v2/user',
+                               headers=headers,
+                               params=payload).json()
+
+    return r_getdevice
